a3ea0e65471ecc00b188c236dca9271847389b10
shadi38485 <shadisalehi38485@gmail.com>
2024-05-17 12:48:41 +0200

    set form data

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,21 +1,87 @@
 import React, { useState } from "react";
 import "./LoginForm.css";
+import { useNavigate } from "react-router-dom";
+
 
 
   const LoginForm = () => {
+    const navigate =useNavigate();
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
     email: "",
     password: "",
-    skills: [],
+    // skills: [],
   });
+  const [errors, setErrors]=useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    // skills: [],
+  });
+
 
   const FormHandle = (e) => {
     e.preventDefault();
-  };
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      skills,
+    } = formData;
+  
+    const errorsValue ={}
+    if (!firstName.trim) {
+      errorsValue.firstName = "firstName is required";
+    }
+    if (!lastName) {
+      errorsValue.lastName = "lastName is required";
+    }
+    if (!email) {
+      errorsValue.email = "email is required";
+    }
+    if (!password) {
+      errorsValue.password = "password is required";
+    }
+    if (Object.keys(errorsValue).length > 0) {
+      setErrors(errorsValue)
+    }else{
+      fetch("https://fakestoreapi.com/users", {
+        method: 'POST',
+        body: JSON.stringify(formData),
+      
+      })
+      
+      .then ((res)=> res.json())
+      .then ((data)=> console.log(data))
+      
+      .catch((error)=> console.log(error))
+      alert("ok");
+      navigate("/LoginPage")
+      
+    }
+  }
+
   const inputHandle =(e)=>{
-    setFormData({ ...formData ,[e.target.id]:e.target.value})
+    setFormData({ ...formData ,[e.target.id]:e.target.value.trim()});
+
+  
+  const checkboxHandle = (e)=>{
+      if(e.target.checked){
+        setFormData({...formData,
+          skills:[...formData.skills,e.target.value]
+        });
+        }else{
+          setFormData({
+            ...formData,
+            skills:formData.skills.filter((skill)=>
+            skill !== e.target.value)
+
+          });
+        
+    
   }
   return (
     <form action="" className="form" onSubmit={FormHandle}>
@@ -32,7 +98,7 @@ import "./LoginForm.css";
           className="input-text"
           onChange={inputHandle}
         />
-        <div className="firstName-error error"></div>
+        <div className="firstName-error error">{errors.firstName}</div>
       </div>
 
       <div className="form-group">
@@ -46,7 +112,7 @@ import "./LoginForm.css";
           className="input-text"
           onChange={inputHandle}
         />
-        <div className="lastName-error error"></div>
+        <div className="lastName-error error">{errors.lastName}</div>
       </div>
 
       <div className="form-group">
@@ -60,7 +126,7 @@ import "./LoginForm.css";
           className="input-text"
           onChange={inputHandle}
         />
-        <div className="email-error error"></div>
+        <div className="email-error error">{errors.email}</div>
       </div>
 
       <div className="form-group">
@@ -76,7 +142,7 @@ import "./LoginForm.css";
             className="input-text"
             onChange={inputHandle}
           />
-          <i className="fa-solid fa-eye" id="icon-password"></i>
+          <i className="fa-solid fa-eye" id="icon-password">{errors.password}</i>
         </div>
       </div>
 
@@ -87,15 +153,21 @@ import "./LoginForm.css";
 
         <div>
           <div className="radio-box">
-            <input type="radio" />
-            <label htmlFor="">html</label>
+            <input type="radio"
+            value="html"
+            onChange={checkboxHandle}
+            />
+            <label htmlFor="">html</label >
           </div>
           <div className="radio-box">
-            <input type="radio" />
+            <input type="radio" 
+            value="css"
+            onChange={checkboxHandle}
+            />
             <label htmlFor="">css</label>
           </div>
           <div className="radio-box">
-            <input type="radio" />
+            <input type="radio" value="js" onChange={checkboxHandle} />
             <label htmlFor="">js</label>
           </div>
         </div>
@@ -106,6 +178,9 @@ import "./LoginForm.css";
       </div>
     </form>
   );
-};
+}
+  }}
+  
 
 export default LoginForm;
+ 
